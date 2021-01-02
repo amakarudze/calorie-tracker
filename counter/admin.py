5f4e6b92ffba8a440aebed635768f717e0521ddd
Anna Makarudze <amakarudze@gmail.com>
2021-01-02 13:27:13 +0200

    Add Consume model and submit food consumed by user

--- counter/admin.py
+++ counter/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Food
+from .models import Consume, Food
 # Register your models here.
 
+admin.site.register(Consume)
 admin.site.register(Food)
--- counter/views.py
+++ counter/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
-from .models import Food
+from .models import Consume, Food
 # Create your views here.
 
 
 def index(request):
+    if request.method == "POST":
+        food_consumed = request.POST['food_consumed']
+        consume = Food.objects.get(name=food_consumed)
+        user = request.user
+        consume = Consume(user=user, food_consumed=consume)
+        consume.save()
+
     foods = Food.objects.all()
     return render(request, 'counter/index.html', {'foods': foods})
